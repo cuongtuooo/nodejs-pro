dcbfa2d6c7d339daaaec3826752de04b54e6ab3a
cuongtuooo <cuonganh1004cva@gmail.com>
2025-05-02 20:50:45 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello word")
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+});
+
+app.get("/cuong", (req, res) => {
+    res.send("Hello cuong")
+});
+
+
+app.listen(8080, () => {
+    console.log(`My App  is running on port: ${PORT}`)
+})
